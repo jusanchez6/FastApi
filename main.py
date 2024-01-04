63fff92f7382931ccfcdf17ccc6e5de80e593ac5
juliansanchez6 <julian.sanchez6@udea.edu.co>
2024-01-04 16:10:54 -0500

    Creación de las peticiones get

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-
 async def root():
     return "¡Hello World!"
+
+@app.get("/url")
+async def root():
+    return {"url_curso": "https://platzi.com/cursos/fastapi/"}
+
